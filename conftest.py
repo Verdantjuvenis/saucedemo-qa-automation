bf812fbf4edc57544d3d4fb815fe09d2d935f89b
Verdantjuvenis <115440105+Verdantjuvenis@users.noreply.github.com>
2026-05-12 20:06:12 -0700

    Enable headless browser for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -7,7 +7,10 @@ from pages.login_page import LoginPage
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     yield driver
