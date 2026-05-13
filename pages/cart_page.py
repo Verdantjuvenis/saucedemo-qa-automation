6885386acc4acc330e73f02eaa37647c10381118
Verdantjuvenis <115440105+Verdantjuvenis@users.noreply.github.com>
2026-05-12 21:05:52 -0700

    Fix checkout page methods and stabilize CI tests

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,19 +1,31 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class CartPage:
     CART_LINK = (By.CLASS_NAME, "shopping_cart_link")
     REMOVE_BACKPACK_BUTTON = (By.ID, "remove-sauce-labs-backpack")
     CART_BADGES = (By.CLASS_NAME, "shopping_cart_badge")
+    CHECKOUT_BUTTON = (By.ID, "checkout")
 
     def __init__(self, driver):
         self.driver = driver
 
     def open(self):
-        self.driver.find_element(*self.CART_LINK).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CART_LINK)
+        ).click()
 
     def remove_backpack(self):
-        self.driver.find_element(*self.REMOVE_BACKPACK_BUTTON).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.REMOVE_BACKPACK_BUTTON)
+        ).click()
 
     def get_badges(self):
         return self.driver.find_elements(*self.CART_BADGES)
+
+    def click_checkout(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CHECKOUT_BUTTON)
+        ).click()
--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -17,27 +17,35 @@ class CheckoutPage:
         self.driver = driver
 
     def start_checkout(self):
-        self.driver.find_element(*self.CHECKOUT_BUTTON).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CHECKOUT_BUTTON)
+        ).click()
 
     def enter_first_name(self, first_name):
-        self.driver.find_element(*self.FIRST_NAME_INPUT).send_keys(first_name)
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.FIRST_NAME_INPUT)
+        ).send_keys(first_name)
 
     def enter_last_name(self, last_name):
-        self.driver.find_element(*self.LAST_NAME_INPUT).send_keys(last_name)
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.LAST_NAME_INPUT)
+        ).send_keys(last_name)
 
     def enter_postal_code(self, postal_code):
-        self.driver.find_element(*self.POSTAL_CODE_INPUT).send_keys(postal_code)
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.POSTAL_CODE_INPUT)
+        ).send_keys(postal_code)
+
+    def fill_checkout_info(self, first_name, last_name, postal_code):
+        self.enter_first_name(first_name)
+        self.enter_last_name(last_name)
+        self.enter_postal_code(postal_code)
 
     def continue_checkout(self):
         self.driver.find_element(*self.CONTINUE_BUTTON).click()
 
     def finish_checkout(self):
         self.driver.find_element(*self.FINISH_BUTTON).click()
-
-    def fill_checkout_info(self, first_name, last_name, postal_code):
-        self.enter_first_name(first_name)
-        self.enter_last_name(last_name)
-        self.enter_postal_code(postal_code)
 
     def get_success_message_element(self):
         return WebDriverWait(self.driver, 10).until(
